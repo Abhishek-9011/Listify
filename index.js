3326424baed879679d232959e8372d3e6cf1a163
Abhishek singh <abhishek774901@gmail.com>
2024-10-28 23:05:39 +0530

    Add new Functionality to add element

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const tasks = [];
+
+(function addElement() {
+  const addButton = document.getElementById("add");
+  addButton.addEventListener("click", function () {
+    const task = document.getElementById("input-box").value;
+    if (task.trim() === "") {
+      alert("Please enter a task");
+      return;
+    }
+
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    const list = document.getElementById("list");
+    list.appendChild(newTask);
+
+    document.getElementById("input-box").value = "";
+  });
+})();
+
